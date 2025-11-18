eaa6efbb6b5311af9a58b8d188e69b849d546d56
CYBER_KARABO <karaboemma25@gmail.com>
2025-11-18 11:38:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuToggle =
+document.getElementById('menuToggle');
+const navLinks =
+document.getElementById('navLinks');
+
+menuToggle.addEventListener('click', () =>{
+    navLinks.classList.toggle('active')
+});
+
+document.getElementById("myButton").addEventListener("click", function () {
+  alert("Join kaysociety against the threats or be a victim, contacts provided below thank you, its on you friend!!! :) ");
+});
+
